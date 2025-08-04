e69fc7018319b4397917e8852c1e3d2908a48936
Petr Slachta <petr.slachta@atlas.cz>
2025-08-04 23:11:55 +0200

    Removing test code

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import Routes from 'routes';
 import ThemeCustomization from 'themes';
 import ScrollTop from 'components/ScrollTop';
 
-// temp TODO - remove after testing
-import 'client/client'
-console.log("process.env.REACT_APP_API_URL =", process.env.REACT_APP_API_URL);
+
 
 // ==============================|| APP - THEME, ROUTER, LOCAL  ||============================== //
 
--- src/menu-items/index.js
+++ src/menu-items/index.js
@@ -3,10 +3,6 @@ import auth from './auth';
 import albums from './albums';
 import pages from './pages';
 
-// temp TODO - remove after testing
-import 'client/client'
-console.log("process.env.REACT_APP_API_URL =", process.env.REACT_APP_API_URL);
-
 
 // ==============================|| MENU ITEMS ||============================== //
 
